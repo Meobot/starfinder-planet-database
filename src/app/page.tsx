60d2db0e0b6b7172cd7d8d086b8dc0dbe568c7ee
Meobot <nathanrhodes312@gmail.com>
2023-08-16 18:06:13 -0400

    rendered planet list for sanity check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,34 @@
-
-import { getDataFromField } from "firebase-config.js"
+"use client"
+import { getDataFromField, getPlanetsCollection } from "firebase-config.js"
+import Navbar from "../components/Navbar"
+import { useEffect, useState } from "react"
 
 export default function Home() {
+	const [planetsList, setPlanetsList] = useState([])
+
+	useEffect(() => {
+		const fetchPlanets = async () => {
+			try {
+				const planets = await getPlanetsCollection()
+				setPlanetsList(planets)
+			} catch (error) {
+				console.log(error)
+			}
+		}
+
+		fetchPlanets()
+
+	}, [])
 
   	return (
 		<div>
-			<h1></h1>
+			<h1>Planetfinder</h1>
+			{planetsList.map((planet, index) => (
+				<div key={index}>
+					{planet.name}
+				</div>
+			))}
+			{/* <Navbar /> */}
 		</div>
 	)
 }
