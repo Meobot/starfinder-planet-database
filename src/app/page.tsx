6257e1fc552bfeb8c0812423b4564efa8a3a4416
Meobot <nathanrhodes312@gmail.com>
2023-08-15 22:42:53 -0400

    removed unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
 
 import { getDataFromField } from "firebase-config.js"
-import { addDataObjectToDocument } from "firebase-config.js"
-import { planetData } from "../planetData"
 
 export default function Home() {
-
-	planetData.forEach(planet => {
-		addDataObjectToDocument(planet.name, {...planet})
-	})	
 
   	return (
 		<div>
