164f99d3e6a8502cee32caca5e7d91664cfbd0dc
Meobot <nathanrhodes312@gmail.com>
2023-08-28 20:48:11 -0400

    added Header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 "use client"
+import Header from "../components/Header"
 import Navbar from "../components/Navbar"
 
 export default function Home() {
 
   	return (
 		<div>
-			<h1>Planetfinder</h1>
+			<Header />
 			<Navbar />
 		</div>
 	)
